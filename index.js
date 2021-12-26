55938e3db8d515dfd300143e603e55110563b0ad
Rinor Luzha <rl51842@ubt-uni.net>
2021-12-26 14:35:59 +0100

    fixed select bug

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const addPlOnePoint = document.querySelector('#plOnePoint');
 const addPlTwoPoint = document.querySelector('#plTwoPoint');
-const reset = document.querySelector('#reset');
+const resetBtn = document.querySelector('#reset');
 
 const plOneScore = document.querySelector('#firstPlayer');
 const plTwoScore = document.querySelector('#secondPlayer');
@@ -10,10 +10,10 @@ const buttons = document.querySelector('.buttons');
 addPlOnePoint.addEventListener('click', () => {
     if (parseInt(plOneScore.innerText) < parseInt(playTo.value) &&
         parseInt(plTwoScore.innerText) < parseInt(playTo.value)) {
-        let i = parseInt(plOneScore.innerText);
-        i++;
-        plOneScore.innerText = i;
-        if (i == parseInt(playTo.value)) {
+        let score = parseInt(plOneScore.innerText);
+        score++;
+        plOneScore.innerText = score;
+        if (score == parseInt(playTo.value)) {
             plOneScore.style.color = 'rgb(39, 192, 133)';
             plTwoScore.style.color = 'rgb(240, 80, 74)';
             buttons.classList.add('disabled');
@@ -23,20 +23,22 @@ addPlOnePoint.addEventListener('click', () => {
 addPlTwoPoint.addEventListener('click', () => {
     if (parseInt(plOneScore.innerText) < parseInt(playTo.value) &&
         parseInt(plTwoScore.innerText) < parseInt(playTo.value)) {
-        let i = parseInt(plTwoScore.innerText);
-        i++;
-        plTwoScore.innerText = i;
-        if (i == parseInt(playTo.value)) {
+        let score = parseInt(plTwoScore.innerText);
+        score++;
+        plTwoScore.innerText = score;
+        if (score == parseInt(playTo.value)) {
             plTwoScore.style.color = 'rgb(39, 192, 133)';
             plOneScore.style.color = 'rgb(240, 80, 74)';
             buttons.classList.add('disabled');
         }
     }
 })
-reset.addEventListener('click', () => {
+const reset = () => {
     plOneScore.innerText = 0;
     plTwoScore.innerText = 0;
     plTwoScore.style.color = 'black';
     plOneScore.style.color = 'black';
     buttons.classList.remove('disabled');
-})
+}
+resetBtn.addEventListener('click', reset);
+playTo.addEventListener('change', reset);
